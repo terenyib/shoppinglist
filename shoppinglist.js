e164d0456823fef23079ff38d724eba30d29a445
balazs.terenyi@hu.ibm.com <balazs.terenyi@hu.ibm.com>
2018-06-07 11:15:50 +0200

    kihúzott lista hozzáadása

--- shoppinglist.js
+++ shoppinglist.js
@@ -15,7 +15,8 @@ M = (function () {
         },
 
         searchItem: function (d) {
-            return lista.map(c => c.text).indexOf(d);
+            //return lista.map(c => c.text).indexOf(d);
+            return lista.findIndex(c => c.text === d);
         },
 
         deleteItem: function (d) {
@@ -32,13 +33,11 @@ M = (function () {
 })();
 
 V = (function () {
-    function ujTermekTorlesGomb() {
-        var myNodelist = document.getElementsByTagName('li');
+    function ujTermekTorlesGomb(li) {
         var span = document.createElement('SPAN');
-        var txt = document.createTextNode('x');
         span.className = 'eltavolitgomb';
-        span.appendChild(txt);
-        myNodelist[myNodelist.length - 1].appendChild(span);
+        span.appendChild(document.createTextNode('x'));
+        li.appendChild(span);
     }
 
     return {
@@ -57,7 +56,7 @@ V = (function () {
             li.style.textDecoration = 'none';
             li.appendChild(document.createTextNode(ujelem));
             document.getElementById('bevasarlolista').appendChild(li);
-            ujTermekTorlesGomb();
+            ujTermekTorlesGomb(li);
         },
 
         termekEltavolitas: function (elem) {
@@ -67,11 +66,16 @@ V = (function () {
 
         termekKihuzas: function (elem) {
             var item = document.getElementById(elem);
+            var celLista;
             if (item.style.textDecoration == 'line-through') {
                 item.style.textDecoration = 'none';
+                celLista = 'bevasarlolista';
             } else {
                 item.style.textDecoration = 'line-through';
+                celLista = 'kihuzottlista';
             }
+            item.parentNode.removeChild(item);
+            document.getElementById(celLista).appendChild(item);
         },
     };
 })();
@@ -107,6 +111,15 @@ C = (function (MObj, VObj) {
         VObj.termekKihuzas(termek);
     }
 
+    var listaKatt = function (e) {
+        if (e.target && e.target.matches('li.termek')) {
+            termekAthuzas(e.target.id);
+        }
+        if (e.target && e.target.matches('span')) {
+            termekTorles(e.target.parentElement.id);
+        }
+    };
+
     return {
         init: function () {
             if (window.localStorage) {
@@ -122,14 +135,8 @@ C = (function (MObj, VObj) {
                 }
             }
             document.getElementById('hozzaadgomb').addEventListener('click', termekEllenorzes);
-            document.getElementById('bevasarlolista').addEventListener('click', function (e) {
-                if (e.target && e.target.matches('li.termek')) {
-                    termekAthuzas(e.target.id);
-                }
-                if (e.target && e.target.matches('span')) {
-                    termekTorles(e.target.parentElement.id);
-                }
-            });
+            document.getElementById('bevasarlolista').addEventListener('click', listaKatt);
+            document.getElementById('kihuzottlista').addEventListener('click', listaKatt);
             document.addEventListener('keypress', function (e) {
                 var key = e.which || e.keyCode;
                 if (key === 13) { termekEllenorzes(); }
